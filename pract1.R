3dd011f1cbfec2a767f76b463949b04f08b5a036
UthejReddy <43196841+UthejReddy@users.noreply.github.com>
2020-02-18 14:34:54 +0530

    Update pract1.R

--- pract1.R
+++ pract1.R
@@ -1,12 +1,12 @@
-2+2
-2+3*4
-3^3
-exp(1)
-sqrt(10)
-pi
+2+2      #for normal addition
+2+3*4    #for addition and multiplication
+3^3      #to find cube root of 3
+exp(1)   #to find the exponential of number
+sqrt(10) #to find squareroot of number
+pi       #value of pi
 2*pi*6378
 
-x<- 1
+x<- 1    #to assign the value 
 y<-3
 z<-4
 
@@ -14,7 +14,7 @@ x*y*z
 
 This.Year<- 2020
 This.Year
-vect<-c(1,2,3,4,5)
+vect<-c(1,2,3,4,5) #to assign multiple values 
 
 x<-c(2,0,0,4)
 y<-c(1,9,9,9)
@@ -27,16 +27,18 @@ sqrt(x)
 x[1]
 x[-1]
 
-x[1]<-3
+x[1]<-3  #to replace the number from list
 x[-1]<-5
 y
+y<9      #use to show the number which comes under 9
+y[4]=3   #replace 4th element with 3
 y<9
-y[4]=3
-y<9
-y[y<9]=2
-df<-data.frame(x=1:4,y=c("a","b","c","h"))
+y[y<9]=2 #replace the number which are less than 9 with 2
+
+df<-data.frame(x=1:4,y=c("a","b","c","h"))#create a short dataset
 data.frame(height=c(150,160),weight=c(65,75))
-df[2,2]
+
+df[2,2]  #to find the element from 2-row and 2-coloumn
 
 df
 
@@ -49,8 +51,7 @@ df[c(1,3),c(1,2)]
 
 vect1<-c(1,2,3)
 vect2<-c("1","2","3")
-mean(vect1)
+mean(vect1) #to find the mean value of dataaset
 mean(vect2)
 mean(vect)
 vect
-airquality<-datasets::airquality
