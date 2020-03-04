c9c4546eb303babf63cb82b1ef58b3577c083fcd
UthejReddy <43196841+UthejReddy@users.noreply.github.com>
2020-03-04 14:03:14 +0530

    Create KNN.R

---
+++ SupervisedMachineLearning/KNN.R
@@ -0,0 +1,88 @@
+KNN<-KNN[,-1]
+View(KNN)
+
+#table of diagnosis
+table(KNN$diagnosis)
+
+#table or proportions with more informative labels
+round(prop.table(table(KNN$diagnosis))*100,digits = 1)
+
+#create normalization Function
+normalize<-function(x)
+{
+  return((x-min(x))/(max(x)-min(x)))
+}
+
+#normalize the KNN data
+KNN_n<-as.data.frame(lapply(KNN[2:31],normalize))
+KNN_nl<-cbind(KNN_n,KNN$diagnosis)
+
+#create training and test data
+KNN_train<-KNN_n[1:469,]
+KNN_test<-KNN_n[470:569,]
+
+#create labels from training and test data
+KNN_train_labels<-KNN[1:469,1]
+KNN_test_labels<-KNN[470:569,1]
+
+library(class)
+
+KNN_test_pred<-knn(train=KNN_train,test=KNN_test,cl=KNN_train_labels,k=20)
+
+##Evaluating model performance
+#load the "gmodel" library
+library(gmodels)
+
+#create the cross tabulation of predicted vs. actual
+CrossTable(x=KNN_test_labels,
+           y=KNN_test_pred,
+           prop.chisq = FALSE,
+           prop.c = FALSE,prop.r = FALSE)
+
+
+####Improving model performance
+#use the Scale() function to z-score standardize a data frame
+KNN_z<-as.data.frame(scale(KNN[-1]))
+
+#crate training and test datasets
+KNN_train<-KNN_z[1:469,]
+KNN_test<-KNN_z[470:569,]
+
+
+KNN_test_pred<-knn(train=KNN_train,test=KNN_test,cl=KNN_train_labels,k=21)
+
+
+#create the cross tabulation of predicted vs. actual
+CrossTable(x=KNN_test_labels,
+           y=KNN_test_pred,
+           prop.chisq = FALSE,
+           prop.c = FALSE,prop.r = FALSE)
+
+
+####tri several different values for k
+
+KNN_test_pred<-knn(train=KNN_train,test=KNN_test,cl=KNN_train_labels,k=2)
+CrossTable(x=KNN_test_labels,
+           y=KNN_test_pred,
+           prop.chisq = FALSE,
+           prop.c = FALSE,prop.r = FALSE)
+
+KNN_test_pred<-knn(train=KNN_train,test=KNN_test,cl=KNN_train_labels,k=5)
+CrossTable(x=KNN_test_labels,
+           y=KNN_test_pred,
+           prop.chisq = FALSE,
+           prop.c = FALSE,prop.r = FALSE)
+
+KNN_test_pred<-knn(train=KNN_train,test=KNN_test,cl=KNN_train_labels,k=14)
+CrossTable(x=KNN_test_labels,
+           y=KNN_test_pred,
+           prop.chisq = FALSE,
+           prop.c = FALSE,prop.r = FALSE)
+
+KNN_test_pred<-knn(train=KNN_train,test=KNN_test,cl=KNN_train_labels,k=30)
+CrossTable(x=KNN_test_labels,
+           y=KNN_test_pred,
+           prop.chisq = FALSE,
+           prop.c = FALSE,prop.r = FALSE)
+
+
